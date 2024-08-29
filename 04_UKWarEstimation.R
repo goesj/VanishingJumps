3d9d589046f893239c30a532f223a4e3a2f0dca3
’goesj’ <goesjulius@gmail.com>
2024-08-29 14:56:10 +0200

    Adjust File for Liu-Li

--- 04_UKWarEstimation.R
+++ 04_UKWarEstimation.R
@@ -13,182 +13,237 @@ load(file = file.path(getwd(),"Data/UKWARData.RData")) # Load Data
 #Estimating Parameters of both the Liu-Li model, as well as our Model 
 #Route II Estimation Method in use (Mortality Improvements)
 
-## Create Data and Constants for Nimble Models 
-NimbleConst <- list("N_AgeGroups"=nrow(ZMatWar),
-                    "N_Year"=ncol(ZMatWar))
+LastYearObs <- 1943 #Adjust here for different scenario
+LastYearObsInd <- which(1901:2010 == LastYearObs)
 
-NimbleData <- list("ZMat"=ZMatWar) #Mortality Improvement Rates
+#After World WAR I, 
+ZMatWar_NOWWII <- ZMatWar[,1:LastYearObsInd]
+NimbleConst_UK <- list("N_AgeGroups"=nrow(ZMatWar_NOWWII),
+                       "N_Year"=ncol(ZMatWar_NOWWII))
 
-xlength <- nrow(ZMatWar) #Amount of Age Groups 
-tlength <- ncol(ZMatWar) #Amount of Periods 
+NimbleData_UK <- list("ZMat"=ZMatWar_NOWWII) 
 
-############ 2.1. Estimation of the Liu-Li Model ############################### 
-
-## Liu, Li Model
+xlength <- nrow(ZMatWar_NOWWII)
+klength <- ncol(ZMatWar_NOWWII)+1
 
 #### ATTENTION ! CHANGE PRIORS IN NIMBLE CODE TO THE ONES SHOWN IN THE PAPER ###
+### 2.1.Own Model AR  ##########################################################
 
-#2.1.1 Initialize nimble Model
-LiuLiModel_UK <- nimbleModel(code=LiuLi_Model, 
-                        constants = NimbleConst, 
-                        data=NimbleData, 
-                        buildDerivs = FALSE)
+### !!! Change constraint, that N_T = 0 !!!!!!!!!! 
 
-#2.1.2 configure nimble Model, to change samplers
-cLiuLiModel_UK <- configureMCMC(LiuLiModel_UK,monitors = c("sigma_eps",
-                                                  "beta","betaJump",
-                                                  "N_t","p",
-                                                  "sigma_time","sdY","muY",
-                                                  "drift",
-                                                  "k",
-                                                  "Y_t","J",
-                                                  "mu"
-                                                  ),
-                               enableWAIC = TRUE, useConjugacy = TRUE)
+OwnModel_AR_War <- nimbleModel(code=OwnModel_AR, 
+                            constants = NimbleConst_UK, 
+                            data=NimbleData_UK, 
+                            buildDerivs = TRUE)
 
-#Define Parameters of which to remove samplers
-params_to_remove <-  c(
-  paste0("b1[",1:10,"]"),
-  paste0("b2[",1:10,"]"),
-  "sdY","sigma_time","muY"
-)
+cOwnModel_AR <- configureMCMC(OwnModel_AR_War,monitors = c("sigma_eps",
+                                                          "beta","betaJump",
+                                                          "N_t","p",
+                                                          "sigma_time",
+                                                          "sdY","muY",
+                                                          "drift",
+                                                          "a",
+                                                          "Y_t","J",
+                                                          "b1","b2",
+                                                          "mu","k"),
+                                enableWAIC = TRUE, useConjugacy = FALSE)
 
-#Remove Samplers of these parameters
-cLiuLiModel_UK$removeSamplers(params_to_remove)
-
-#Create New samplers for the removed parameters
-cLiuLiModel_UK$addSampler(target= c("b1[1:10]"),
-                  type="AF_slice")
-cLiuLiModel_UK$addSampler(target= c("b2[1:10]"),
-                  type="AF_slice")
-
-cLiuLiModel_UK$addSampler(nodes = "muY",type="slice")
-cLiuLiModel_UK$addSampler(nodes = "sdY",type="slice")
-cLiuLiModel_UK$addSampler(nodes = "sigma_time",type="slice")
-
-
-#2.1.3 Build nimble model
-bLiuLiModel_UK <- buildMCMC(cLiuLiModel_UK)
-#2.1.4 Compile nimble model 
-comLiuLiModel_UK <- compileNimble(LiuLiModel_UK,bLiuLiModel_UK)
-
-
-#2.1.5 Sample Parameters
-SamplesLiuLi_War <- runMCMC(comLiuLiModel_UK$bLiuLiModel_UK, 
-                       niter = 15000, 
-                       thin=10,
-                       nburnin = 5000, 
-                       nchains = 2) 
-
-#Create Summary Output of Paramters
-SummaryOutput(SamplesLiuLi_War,
-              params=c("beta","betaJump","drift","sigma_time",
-              "sigma_eps","p","muY","sdY")) %>% print(n=150) 
-
-
-######## 2.2 Estimation of Own Model  ########################################## 
-
-# Estimation of Own Model with Vanishing Jumps
-
-#2.2.1 Initialize nimble Model
-OwnModel_UK <- nimbleModel(code=OwnModel, 
-                           constants = NimbleConst, 
-                           data=NimbleData, 
-                           buildDerivs = FALSE)
-
-#2.2.2 Configuration of nimble model 
-cOwnModel_UK <- configureMCMC(OwnModel_UK,
-                                monitors = c("sigma_eps","beta","betaJump",
-                                             "N_t","p","sigma_time",
-                                             "sdY","muY", "drift","a",
-                                             "Y_t","J", "b1","b2",
-                                             "mu","k"),
-                            enableWAIC = TRUE, 
-                            #set to false to speed up configuration
-                            useConjugacy = FALSE) 
-
-#Define Parameters of which to remove samplers
 params_to_remove <-  c(
   "muY","sdY","sigma_time",
   "a","p",
   paste0("b1[",1:xlength,"]"),
   paste0("b2[",1:xlength,"]"),
-  paste0("k[",2:(tlength),"]"),
-  paste0("Y_t[",3:(tlength+1),"]")
+  paste0("Y_t[",1:(klength),"]")
 )
 
-cOwnModel_UK$removeSamplers(params_to_remove) #remove parameters
-cOwnModel_UK$addSampler(nodes = "muY",type="slice")
-cOwnModel_UK$addSampler(nodes = "a",type="slice")
-cOwnModel_UK$addSampler(nodes = "sdY",type="slice")
-#gibbs sampler for p
-cOwnModel_UK$addDefaultSampler(nodes="p", useConjugacy = TRUE)
-cOwnModel_UK$addSampler(nodes = "sigma_time",type="slice")
-#gibbs sampler of k (attention, k1 is a constant)
-cOwnModel_UK$addDefaultSampler(nodes=paste0("k[",2:tlength,"]"),
-                                 useConjugacy = TRUE, print = FALSE)
+cOwnModel_AR$removeSamplers(params_to_remove)
+cOwnModel_AR$addSampler(target = "muY",type="slice")
+cOwnModel_AR$addSampler(target = "a",type="slice")
+cOwnModel_AR$addSampler(target = "sdY",type="slice")
+cOwnModel_AR$addDefaultSampler(nodes="p", useConjugacy = TRUE)
+cOwnModel_AR$addSampler(target = "sigma_time",type="slice")
 
-# slice sampler for Y_t
-for(j in 3:(tlength+1)){ #one time period longer due to differencing
-  cOwnModel_UK$addSampler(target=paste0("Y_t[",j,"]"),
+for(j in 3:(klength)){
+  cOwnModel_AR$addSampler(target=paste0("Y_t[",j,"]"),
                             type="slice")
 }
-cOwnModel_UK$addSampler(target= c("b1[1:10]"),
-                  type="AF_slice")
-cOwnModel_UK$addSampler(target= c("b2[1:10]"),
-                  type="AF_slice")
+
+nimbleHMC::addHMC(cOwnModel_AR, 
+                  target = c("k[2:43]",
+                             "b1[1:10]","b2[1:10]"),
+                  type = "NUTS", replace = TRUE,
+                  control = list(delta = 0.82, maxTreeDepth = 11))
 
 
-#2.2.3 build Model
-bOwnModel_UK <- buildMCMC(cOwnModel_UK)
-#2.2.4 compile Model
-comOwnModel_UK <- compileNimble(OwnModel_UK,bOwnModel_UK)
+bOwnModel_AR <- buildMCMC(cOwnModel_AR)
+comOwnModel_AR <- compileNimble(OwnModel_AR_War, bOwnModel_AR)
 
-#2.2.5 create samples 
-Samples_OwnMod_UK <- runMCMC(comOwnModel_UK$bOwnModel_UK, 
-                       niter = 15000,
-                       thin=10,
-                       nburnin = 5000, 
-                       nchains = 2) #set seed for reproducibility
 
-#Plot Summary Of samples, same column structure as in paper
-SummaryOutput(Samples_OwnMod_UK,
-              params=c("beta","betaJump","drift","sigma_time",
-                       "sigma_eps","p","a","muY","sdY")) %>% print(n=400) #work
+SamplesAR_War_43 <- runMCMC(comOwnModel_AR$bOwnModel_AR, 
+                                niter = 6500,
+                                thin=2,
+                                nburnin = 2500, 
+                                nchains = 2) 
+
+SummaryOutput(SamplesAR_War_43,
+              params=c("betaJump","sigma_time",
+                       "beta","drift","p","sdY","muY",
+                       "a","N_t")) %>% print(n=400) #work
+
+##### 2.2 Liu-Li ###############################################################
+LiuLiModel_War <- nimbleModel(code=LiuLi_Model, 
+                            constants = NimbleConst_UK, 
+                            data=NimbleData_UK, 
+                            buildDerivs = TRUE)
+
+cLiuLiModel <- configureMCMC(LiuLiModel_War,monitors = c("sigma_eps",
+                                                      "beta","betaJump",
+                                                      "N_t","p",
+                                                      "sigma_time","sdY","muY",
+                                                      "drift",
+                                                      "k",
+                                                      "Y_t","J",
+                                                      "mu"), 
+                            enableWAIC = TRUE, useConjugacy = TRUE)
+
+
+params_to_remove <-  c(
+  paste0("b1[",1:10,"]"),
+  paste0("b2[",1:10,"]"),
+  "sdY","sigma_time","muY","p"
+)
+
+cLiuLiModel$removeSamplers(params_to_remove)
+
+nimbleHMC::addHMC(cLiuLiModel, 
+                  target = c("b1[1:10]","b2[1:10]"),
+                  type = "NUTS", replace = TRUE,
+                  control = list(delta = 0.82, maxTreeDepth = 11))
+
+cLiuLiModel$addSampler(target = "muY",type="slice")
+cLiuLiModel$addSampler(target = "sdY",type="slice")
+cLiuLiModel$addSampler(target = "sigma_time",type="slice")
+cLiuLiModel$addDefaultSampler(nodes = "p",print = TRUE)
+
+
+bLiuLiModel <- buildMCMC(cLiuLiModel)
+comLiuLiModel <- compileNimble(LiuLiModel_War,bLiuLiModel)
+
+
+SamplesLiuLi_War_43 <- runMCMC(comLiuLiModel$bLiuLiModel, 
+                                   niter = 8000,
+                                   thin=4,
+                                   nburnin = 4000, 
+                                   nchains = 2) #set seed for reproducibility
+
+SummaryOutput(SamplesLiuLi_War_43,
+              params=c("betaJump","sigma_time",
+                       "beta","drift","p","sdY","muY",
+                       "a","N_t")) %>% print(n=400) #work
+
+##### 2.3 Own Model MA #########################################################
+OwnModel_MA_War <- nimbleModel(code=OwnModel_MA, 
+                            constants = NimbleConst_UK, 
+                            data=NimbleData_UK, 
+                            buildDerivs = TRUE)
+
+cOwnModel_MA <- configureMCMC(OwnModel_MA_War,monitors = c("sigma_eps",
+                                                          "beta","betaJump",
+                                                          "N_t","p",
+                                                          "sigma_time",
+                                                          "sdY","muY",
+                                                          "drift",
+                                                          "b",
+                                                          "Y_t","J",
+                                                          "b1","b2",
+                                                          "mu","k"),
+                              enableWAIC = TRUE, useConjugacy = FALSE)
+
+#For Change of Samplers
+params_to_remove <-  c(
+  "muY","sdY","sigma_time",
+  "b","p",
+  paste0("b1[",1:xlength,"]"),
+  paste0("b2[",1:xlength,"]"),
+  paste0("Y_t[",1:(klength),"]")
+)
+
+cOwnModel_MA$removeSamplers(params_to_remove)
+cOwnModel_MA$addSampler(target = "muY",type="slice")
+cOwnModel_MA$addSampler(target = "b",type="slice")
+cOwnModel_MA$addSampler(target = "sdY",type="slice")
+cOwnModel_MA$addDefaultSampler(nodes="p", useConjugacy = TRUE)
+cDiffModel_Own$addSampler(target = "sigma_time",type="slice")
+# cDiffModel_Own$addDefaultSampler(nodes=paste0("k[",2:(klength -1),"]"),
+#                                  useConjugacy = TRUE, print = FALSE)
+
+for(j in 3:(klength)){
+  cOwnModel_MA$addSampler(target=paste0("Y_t[",j,"]"),
+                            type="slice")
+}
+
+nimbleHMC::addHMC(cOwnModel_MA, 
+                  target = c("k[2:43]",
+                             "b1[1:10]","b2[1:10]"),
+                  type = "NUTS", replace = TRUE,
+                  control = list(delta = 0.82, maxTreeDepth = 11))
+
+
+
+bOwnModel_MA <- buildMCMC(cOwnModel_MA)
+comOwnModel_MA <- compileNimble(OwnModel_MA_War,bOwnModel_MA)
+
+
+SamplesMA_War_43 <- runMCMC(comOwnModel_MA$bOwnModel_MA, 
+                                 niter = 6500,
+                                 thin=2,
+                                 nburnin = 2500, 
+                                 nchains = 2) #set seed for reproducibility
+
+
+SummaryOutput(Samples_OwnMod_War_MA,
+              params=c("sigma_eps","betaJump","sigma_time",
+                       "beta","drift","p","sdY","muY",
+                       "b","N_t")) %>% print(n=400) #work
+
+
+######### 2.4 Lee Carter Model (only for scenario II)) #########################
+LC_War <-  nimbleModel(code = LC_Diff, 
+                       constants = NimbleConst_UK, 
+                       data=NimbleData_UK)
+
+cLC_War <- configureMCMC(LC_War, 
+                         print = TRUE, useConjugacy = TRUE,
+                         monitors = c("k","sigma_eps",
+                                      "beta",
+                                      "sigma_time",
+                                      "drift",#"b1",
+                                      "mu"
+                         ),
+                         enableWAIC = TRUE) #to compare Models
+
+params_to_remove <-  c(
+  paste0("b1[",1:10,"]")
+)
+cLC_War$removeSamplers(params_to_remove)
+cLC_War$addSampler(target= c("b1[1:10]"),
+                   type="AF_slice")
+
+bLC_War <- buildMCMC(cLC_War)
+comLC_War <- compileNimble(LC_War,
+                           bLC_War)
+
+
+SamplesLC_War_80<- runMCMC(comLC_War$bLC_War, 
+                           niter = 4000,
+                           thin=1,
+                           nburnin = 2000, 
+                           nchains = 2)
 
 
 ############# 3. Save Results ##################################################
-save(Samples_OwnMod_UK,
-     SamplesLiuLi_War,
-     file = file.path(getwd(),"Results/Samples_UKWar.RData"))
-
-#alternatively, load existing Samples
-load(file.path(getwd(),"Results/Samples_UKWar.RData"))
-
-########## 3.1 Calculate WAIC Values ###########################################
-LikeMatOwn_UK <- LikelihoodMatrixFun(Samples = do.call(rbind, Samples_OwnMod_UK),
-                                    n = length(ZMatWar),
-                                    ZMat = ZMatWar)
-
-LikeMatLiuLi_UK <- LikelihoodMatrixFun(Samples = do.call(rbind, SamplesLiuLi_War),
-                                    n = length(ZMatWar),
-                                    ZMat = ZMatWar)
-
-WAICOwnUK <- loo::waic(log(LikeMatOwn_UK))
-WAICLiuUK <- loo::waic(log(LikeMatLiuLi_UK))
-
-LOOOwnUK <- loo::loo(log(LikeMatOwn_UK))
-LOOLiuUK <- loo::loo(log(LikeMatLiuLi_UK)) # loo IC = -2 elpd_loo
-
-
-CompDataFrameWar <- data.frame("Model"=c("Own","Liu-Li"),
-                              "LOOCV"= c(LOOOwnUK$estimates[3,1],
-                                         LOOLiuUK$estimates[3,1]),
-                              "WAIC"=c(WAICOwnUK$estimates[3,1],
-                                       WAICLiuUK$estimates[3,1]))
-
-########## 3.2 Save Results ####################################################
-save(CompDataFrameWar,
-     file = file.path(getwd(),"Results/WAIC_UKWar.RData"))
-
+save(SamplesAR_War_43,
+     SamplesLiuLi_War_43,
+     SamplesMA_War_43,
+     file = file.path(getwd(),"Results/Samples_UKWar_43.RData"))
 
